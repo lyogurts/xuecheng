54e9b6bbf8c238a1a67b981175c4262c9a221c0e
Lyogurts <lyogurt@163.com>
2023-03-03 17:24:54 +0800

    2023.03.03课程计划查询修改，添加章节目录小节目录完成

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/TeachplanService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/TeachplanService.java
@@ -1,9 +1,13 @@
 package com.xuecheng.content.service;
 
+import com.xuecheng.content.model.dto.SaveTeachplanDto;
 import com.xuecheng.content.model.dto.TeachplanDto;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 public interface TeachplanService {
     public List<TeachplanDto> findTeachplayTree(Long courseId);
-}
+    public void saveTeachplan( SaveTeachplanDto teachplanDto);
+
+    }
