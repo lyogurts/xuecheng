fb023d53e218176af07b703ed51f18ea5b444dbb
Lyogurts <lyogurt@163.com>
2023-03-03 16:12:07 +0800

    2023.03.03课程计划查询

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/mapper/TeachplanMapper.java
@@ -1,7 +1,11 @@
 package com.xuecheng.content.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.content.model.dto.TeachplanDto;
 import com.xuecheng.content.model.po.Teachplan;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 /**
  * <p>
@@ -11,5 +15,6 @@ import com.xuecheng.content.model.po.Teachplan;
  * @author itcast
  */
 public interface TeachplanMapper extends BaseMapper<Teachplan> {
-
+    //查询课程计划（组成树形结构）
+    public List<TeachplanDto> selectTreeNodes(Long courseId);
 }
